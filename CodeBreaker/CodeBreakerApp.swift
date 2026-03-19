89d5ff8e8b6c61566baab2460c55a4417224677f
charlotte <hey.charlotte@icloud.com>
2026-03-19 22:31:25 +0000

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Charlotte Wheeler on 19/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
