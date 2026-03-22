56b43ad8c3ad9bfa683b476c1f8b20249d0d2b21
charlotte <hey.charlotte@icloud.com>
2026-03-22 14:52:03 +0000

    pegs, glass effect and background image

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -2,13 +2,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, codebreaker!")
+        ZStack {
+            Image("machine")
+            .resizable()
+            .edgesIgnoringSafeArea(.all)
+            VStack {
+                pegs(colors: [PegItem(color: .green), PegItem(color: .green), PegItem(color: .green), PegItem(color: .red)])
+                pegs(colors: [PegItem(color: .yellow), PegItem(color: .blue), PegItem(color: .green), PegItem(color: .red)])
+                pegs(colors: [PegItem(color: .blue), PegItem(color: .blue), PegItem(color: .blue), PegItem(color: .yellow)])
+                
+                pegs(colors: [PegItem(color: .blue), PegItem(color: .blue), PegItem(color: .blue), PegItem(color: .yellow)])
+                pegs(colors: [PegItem(color: .blue), PegItem(color: .blue), PegItem(color: .blue), PegItem(color: .yellow)])
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func pegs(colors: [PegItem]) -> some View {
+        HStack(spacing: 20) {
+            ForEach(colors) { x in
+                GlassPeg(color: x.color)
+            }
+        }
+    }
+}
+
+struct PegItem: Identifiable {
+    let id = UUID()
+    let color: Color
+}
+
+struct GlassPeg: View {
+    var color: Color
+    
+    var body: some View {
+        Circle()
+            .fill(color.gradient.opacity(0.4))
+    
+            .frame(width: 70.0, height: 70.0)
+            .glassEffect()
+            
     }
 }
 
